89b6e724a05cc2760a20bc3840e717cc529c3665
Rafsanjani Muhammod <mrz30792@gmail.com>
2019-01-30 16:53:36 +0600

    Update dendrogramHeatMap.py

--- dendrogramHeatMap.py
+++ dendrogramHeatMap.py
@@ -1,20 +1,25 @@
 import pandas as pd
 import seaborn as sns
 
+### Web: https://docs.scipy.org/doc/scipy/reference/generated/scipy.cluster.hierarchy.linkage.html
+
 #%%
 ### Generate dataset using CSV.py
-D = pd.read_csv('/home/mrz/Documents/out.csv')
+D = pd.read_csv('/home/mrz/binaryOutput.csv')
 D = D.set_index('Genes')
-#%%
-def clusterHeatMapSTD():
-    sns.clustermap(D, standard_scale=1)
 
-def clusterHeatMapZ():
-    sns.clustermap(D, z_score=1)
+#%%
+# method='single' ->    Nearest Point Algorithm. 
+# method='complete' ->  Farthest Point Algorithm or Voor Hees Algorithm.  
+# method='average' ->   UPGMA algorithm
+# method='weighted' ->  WPGMA algorithm
+# method='median' ->    WPGMC algorithm
+# method='centroid' ->  UPGMC algorithm
+
+def clusterHeatMap():
+    sns.clustermap(data=D, method='average')
     
+#%%
+clusterHeatMap()
 
 #%%
-clusterHeatMapSTD()
-
-#%%
-clusterHeatMapZ()
